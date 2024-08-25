863ec9699748ae43b837a7d29fb0d10e404f6966
Julien Griffoul <julien.griffoul@griffoul.in>
2024-08-25 16:32:33 +0200

    :sparkles: Implement CLI to send a Discord message

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token     string
+	ChannelID string
+	Message   string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.StringVar(&ChannelID, "c", "", "Channel ID")
+	flag.StringVar(&Message, "m", "", "Message")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatalf("Error creating Discord session: %v", err)
+		return
+	}
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatalf("Error opening connection: %v", err)
+		return
+	}
+
+	defer dg.Close()
+
+	_, err = dg.ChannelMessageSend(ChannelID, Message)
+	if err != nil {
+		log.Fatalf("Error sending message: %v", err)
+		return
+	}
+
+	fmt.Println("Message sent successfully!")
 }
